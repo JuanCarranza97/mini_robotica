6afa9a944e68286f9ae8fcbeea120eb7c5789ace
Borra <juan15290337@itcg.edu.mx>
2018-10-07 21:49:00 -0500

    Se elimino eipi_follower

--- eipi_follower/GPIO.cpp
+++
@@ -1,9 +0,0 @@
-#include "GPIO.h"
-
-void GPIO_init(void){
-      pinMode(13,OUTPUT);
-      pinMode(4,OUTPUT);
-      pinMode(3,OUTPUT);
-      pinMode(12,INPUT_PULLUP);
-      pinMode(11,INPUT_PULLUP);  
-}
